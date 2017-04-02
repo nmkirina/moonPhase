fc7daf43870c8dd23838da8210f7064292e681e3
Irina Sokolova <user.nmkirina@gmail.com>
2017-04-02 23:18:05 +0300

    find moon age

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container well">
+            <div class="hero-unit">
+                <h1>Вычисление фазы луны по дате</h1>
+                <p>
+                    Выберите дату.
+                </p>
+                <form action="main.php">
+                    <button type="submit" class="btn btn-primary btn-large">Сгенерировать</button>
+                </form>
+                
+            </div>
+        </div>
+    </body>
+</html>
+
+
+
